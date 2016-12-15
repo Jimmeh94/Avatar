1b039513c530411eef82fdd7b69278d90a711d40
Jimmy <jimmeh.business@gmail.com>
2016-12-15 01:10:41 -0600

    Add passive and hostile areas

---
+++ src/main/java/avatar/game/areas/HostileArea.java
@@ -0,0 +1,8 @@
+package avatar.game.areas;
+
+public class HostileArea extends Area {
+    
+    public HostileArea(AreaShape shape, String displayName) {
+        super(shape, displayName);
+    }
+}
---
+++ src/main/java/avatar/game/areas/PassiveArea.java
@@ -0,0 +1,8 @@
+package avatar.game.areas;
+
+public class PassiveArea extends Area {
+
+    public PassiveArea(AreaShape shape, String displayName) {
+        super(shape, displayName);
+    }
+}
